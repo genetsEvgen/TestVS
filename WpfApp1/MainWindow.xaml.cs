3160ac1ddb66f4befcd0ede31b06911a5abbfa71
genets@crm-expert.biz <genets@crm-expert.biz>
2019-08-30 10:42:19 +0500

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            //List<Phone> phonesList = new List<Phone>
+            //{
+            //    new Phone { Title="iPhone 6S", Company="Apple", Price=54990 },
+            //    new Phone {Title="Lumia 950", Company="Microsoft", Price=39990 },
+            //    new Phone {Title="Nexus 5X", Company="Google", Price=29990 }
+            //};
+            //dataGrid.ItemsSource = phonesList;
+        }
+    }
+}
